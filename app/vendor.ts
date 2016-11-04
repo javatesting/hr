c5cd058ce316c778c6344dff412e1c68dc646c2b
MBodnar <kbodnar@ukr.net>
2016-11-04 14:06:25 +0200

    webpack

---
+++ app/vendor.ts
@@ -0,0 +1,19 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/compiler';
+import '@angular/common';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+import '@angular/upgrade';
+
+// RxJS
+import 'rxjs';
+import 'core-js';
+import 'reflect-metadata';
+import 'zone.js';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'vendor': './app/vendor.ts',
+        'main': './app/main.ts'
+    },
+
+    output: {
+        filename: './js/[name].js'
+    },
+
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            // {
+            //     test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+            //     loader: 'file?name=assets/[name].[hash].[ext]'
+            // },
+            {
+                test: /\.css$/,
+                // exclude: helpers.root('src', 'app'),
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                // include: helpers.root('src', 'app'),
+                loader: 'raw'
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['main', 'vendor']
+        }),
+
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+};
